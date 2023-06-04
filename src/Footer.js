21eab5dbeb35a07ad291d18d4b1351f675533515
OksanaKhritonenko <o.khritonenko@gmail.com>
2023-06-04 22:54:59 +0200

    added footer

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <a
+        href="https://github.com/OksanaKhritonenko/weather-react.git"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Open-source code
+      </a>
+      by Oksana Khritonenko
+    </footer>
+  );
+}
