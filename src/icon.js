825e244f0e6e57c3316e71520af7b248f1125c0d
OksanaKhritonenko <o.khritonenko@gmail.com>
2023-06-04 23:11:26 +0200

    changed default color for icon

--- src/icon.js
+++ src/icon.js
@@ -27,7 +27,7 @@ export default function Icon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#1e1e1e"
+      color="#0881a3"
       size={props.size}
       animate={true}
     />
