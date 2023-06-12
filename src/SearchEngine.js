e89e87de5019d2490809c80cb3a48270dfc2c115
OksanaKhritonenko <o.khritonenko@gmail.com>
2023-06-12 22:35:32 +0200

    deleted weatherIcon and updated FormatedDate

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import WeatherData from "./WeatherData";
 import Forecast from "./Forecast";
+
 export default function SearchEngine(props) {
   const [city, setCity] = useState(props.defaultCity);
 
@@ -26,7 +27,7 @@ export default function SearchEngine(props) {
   }
 
   function handleSubmit(event) {
-    let apiKey = "ab8fcfae42fbe1t26ac753doe7000fbb";
+    let apiKey = `ab8fcfae42fbe1t26ac753doe7000fbb`;
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
   }
